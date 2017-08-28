ea66ce366e8bd99441dd09bc80b5664cb2cdde45
Pranav Kumar <pranav.kumar@iontrading.com>
2017-08-28 13:55:48 +0530

    Word Count

---
+++ MapReduce/MapReduce/src/main/java/mapreduce/example/WordCountMapper.java
@@ -0,0 +1,20 @@
+package mapreduce.example;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		String w = value.toString();
+		String data[] = w.split(" ");
+		for (String s : data) {
+			context.write(new Text("COUNT"), new IntWritable(1));
+		}
+	}
+
+}
