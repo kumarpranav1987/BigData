62a5bb75b89e5a6073d5b2ea504776655a6b9bbf
Pranav Kumar <pranav.kumar@iontrading.com>
2017-09-01 18:33:07 +0530

    MIn Temmp

--- Spark/Spark_Demo/src/main/spark/learn/spark/CalculateMinTemprature.scala
+++ Spark/Spark_Demo/src/main/spark/learn/spark/CalculateMinTemprature.scala
@@ -4,7 +4,8 @@ import org.apache.spark.SparkContext
 
 object CalculateMinTemprature {
   def main(args: Array[String]): Unit = {
-    val sc = new SparkContext("local[*]","MinTemprature");
-    
+    val sc = new SparkContext("local[*]", "MinTemprature")
+    val rdd = sc.textFile("1800.csv").filter { x => { x.split(",")(2) == "TMIN" } }.map { x => { (x.split(",")(0), x.split(",")(3).toInt) } }.reduceByKey((x, y) => { Math.min(x, y) });
+    rdd.foreach(println)
   }
 }
